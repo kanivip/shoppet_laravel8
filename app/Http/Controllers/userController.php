370360bcf9c34010bb2c3d8152cad57c5f5b9396
unknown <kanivip284@gmail.com>
2021-05-12 17:28:30 +0700

    register finished

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Http\Request;
 
+use Illuminate\Http\Request;
+use DB;
+use App\Models\user;
+use Illuminate\Support\Facades\Hash;
 class userController extends Controller
 {
     public function showLogin(){
@@ -17,11 +20,24 @@ class userController extends Controller
         $validated = $request->validate([
             'firstName' => 'required|string|max:30',
             'lastName' => 'required|string|max:20',
-            'email' => 'required|email:rfc,dns|max:255',
+            'email' => 'required|email:rfc,dns|max:255|unique:user,gmail',
             'phoneNumber' => 'required|string|max:20|min:10',
             'password' => 'required|string|max:100|min:8',
             'rePassword' => 'required|same:password|string|min:8|max:100',
         ]);
-        return view('pages.index');
+
+        $user = new user;
+        $user->gmail = $request->email;
+        $user->first_name = $request->firstName;
+        $user->last_name = $request->lastName;
+        $user->phone_number = $request->phoneNumber;
+        $user->password = hash::make($request->password);
+        $user->save();
+        
+        return view("pages.login");
+        
+    }
+    public function doLogin(Request $request){
+        
     }
 }
